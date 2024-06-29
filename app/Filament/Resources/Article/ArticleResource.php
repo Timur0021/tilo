aca79673ba45de6a5d54850188fb89ae8293947c
tima <tima.stella88@gmail.com>
2024-06-29 11:20:56 +0300

    add article table

--- app/Filament/Resources/Article/ArticleResource.php
+++ app/Filament/Resources/Article/ArticleResource.php
@@ -9,9 +9,11 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Auth;
 
 class ArticleResource extends Resource
 {
@@ -31,7 +33,32 @@ class ArticleResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\Section::make('Article Information')
+                    ->description('This is the information about the article.')
+                    ->schema([
+                        Forms\Components\TextInput::make('title')
+                            ->label('Title')
+                            ->required()
+                            ->placeholder('Enter Article Title'),
+                        Forms\Components\MarkdownEditor::make('description')
+                            ->label('Description')
+                            ->required()
+                            ->placeholder('Enter Article Description'),
+                        Forms\Components\Checkbox::make('published')
+                            ->label('Published')
+                            ->default(false),
+                        Forms\Components\Hidden::make('user_id')
+                            ->default(Auth::id()),
+                    ]),
+                Forms\Components\Section::make('Image Article')
+                    ->description('This is the image about the article.')
+                    ->schema([
+                        Forms\Components\SpatieMediaLibraryFileUpload::make('image_article')
+                            ->label('Icon image')
+                            ->collection('image_article')
+                            ->image()
+                            ->imageEditor(),
+                    ])->columnSpan(2),
             ]);
     }
 
@@ -39,7 +66,23 @@ class ArticleResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')
+                    ->label('ID'),
+                TextColumn::make('title')
+                    ->label('Title'),
+                TextColumn::make('description')
+                    ->label('Description'),
+                Tables\Columns\SpatieMediaLibraryImageColumn::make('image_article')
+                    ->disk('public')
+                    ->collection('image_article')
+                    ->circular(),
+                Tables\Columns\IconColumn::make('published')
+                    ->label('Published')
+                    ->boolean(),
+                TextColumn::make('user.name')
+                    ->label('User'),
+                TextColumn::make('created_at')
+                    ->label('Created at'),
             ])
             ->filters([
                 //
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,10 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Spatie\Image\Enums\Fit;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Article extends Model
+class Article extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     public $fillable = [
         'title',
@@ -19,6 +24,18 @@ class Article extends Model
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function media(): MorphMany
+    {
+        return $this->morphMany(Media::class, 'model');
+    }
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('preview')
+            ->fit(Fit::Contain, 1920, 1080)
+            ->nonQueued();
     }
 }
